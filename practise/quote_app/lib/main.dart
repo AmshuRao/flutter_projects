ccad583ee23aa0ad5f2ad816b0cf495b4f7af51d
Amshu G M <amshugm2003@gmail.com>
2024-01-10 23:07:43 +0530

    add quote button added using dialogue box

--- practise/quote_app/lib/main.dart
+++ practise/quote_app/lib/main.dart
@@ -1,13 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'quotes.dart';
 import 'quote_card.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:QuoteApp(),
+    home: QuoteApp(),
   ));
 }
 
@@ -20,37 +18,108 @@ class QuoteApp extends StatefulWidget {
 
 class _QuoteAppState extends State<QuoteApp> {
   List<Quotes> quotes = [
-    Quotes(author: "amshu G M",quote: "chaos is a ladder"),
-    Quotes(author: "amshu G M",quote: "chaos is a ladder"),
-    Quotes(author: "amshu G M",quote: "chaos is a ladder"),
+    Quotes(author: "amshu G M", quote: "chaos is a ladder"),
+    Quotes(author: "amshu G M", quote: "chaos is a ladder"),
+    Quotes(author: "amshu G M", quote: "chaos is a ladder"),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[600],
-      appBar:AppBar(
-        title: Text("Quote APP",
-          style:TextStyle(
-            color: Colors.amberAccent
-          )
+        backgroundColor: Colors.grey[600],
+        appBar: AppBar(
+          title: Text("Quote APP",
+              style: TextStyle(
+                  color: const Color.fromRGBO(255, 215, 64, 1))),
+          backgroundColor: Colors.grey[800],
+          centerTitle: true,
         ),
-        backgroundColor: Colors.grey[800],
-        centerTitle: true,
-      ),
-      body:Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: 
-          (quotes.map((quote)=>MakeCard(
-            quote:quote,
-            delete:(){
-              setState((){
-                quotes.remove(quote);
-              });
-            }
-          ))).toList()
-      ,
-      )
-    );
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ...quotes
+                .map((quote) => MakeCard(
+                    quote: quote,
+                    delete: () {
+                      setState(() {
+                        quotes.remove(quote);
+                      });
+                    }))
+                .toList(),
+            Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 0.0),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateColor.resolveWith(
+                            (states) => Colors.amber)),
+                    onPressed: () {
+                      String? author;
+                      String? quote;
+                      final GlobalKey<FormState> formKey =
+                          GlobalKey<FormState>();
+
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Add a quote'),
+                            content: Container(
+                              padding: EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Form(
+                                key: formKey,
+                                child: Column(
+                                  children: <Widget>[
+                                    TextFormField(
+                                      style: TextStyle(fontSize: 14.0),
+                                      decoration: InputDecoration(
+                                        hintText: 'Author',
+                                      ),
+                                      onSaved: (String? value) {
+                                        author = value;
+                                      },
+                                    ),
+                                    SizedBox(height: 8.0),
+                                    TextFormField(
+                                      style: TextStyle(fontSize: 14.0),
+                                      decoration: InputDecoration(
+                                        hintText: 'Quote',
+                                      ),
+                                      onSaved: (String? value) {
+                                        quote = value;
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('Submit'),
+                                onPressed: () {
+                                  if (formKey.currentState?.validate() ??
+                                      false) {
+                                    formKey.currentState?.save();
+                                    setState(() {
+                                      if (quotes.length < 5) {
+                                        quotes.add(Quotes(
+                                            author: author!, quote: quote!));
+                                      }
+                                    });
+                                    Navigator.of(context).pop();
+                                  }
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Text("add items")))
+          ],
+        ));
   }
 }
